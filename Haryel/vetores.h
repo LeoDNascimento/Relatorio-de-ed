aafd9c5632f43ec2086dcbed1ab9d8d49e8f0cc6
Haryel Ramalho <haryelgillet@gmail.com>
2019-03-09 14:40:59 -0300

    Atualizando

--- Haryel/vetores.h
+++ Haryel/vetores.h
@@ -15,17 +15,13 @@ void showArray(int *array, int size){
 
 	srand(time(NULL));
 
-	
-
 }*/
+ 
+int* aloca_vetor(int *array, int size){
+	
+	*array = (int *) malloc(size * sizeof(int));
 
-int* aloca_vetor(int size){
-
-	int *array;
-
-	array = (int *) malloc(size * sizeof(int));
-
-	if(array == NULL){
+	if(*array == NULL){
 		printf("Erro: Sem memoria!\n");
 		exit(1);
 	}
@@ -37,7 +33,7 @@ int* gera_vetor_alt(int size){
 
 	srand(time(NULL));
 
-	int *array = aloca_vetor(size);
+	int *array = aloca_vetor(array, size);
 
 	for (int i = 0; i <= size; i++){
 		array[i] = rand() % 100;
